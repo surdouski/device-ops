ff4e20e5b0adc7a3e5f1dbbc82a812e5ec1a8537
surdouski <michael.surdouski@gmail.com>
2024-10-03 11:47:33 -0400

    more defaults, use env vars, etc

--- device_ops/cli.py
+++ device_ops/cli.py
@@ -1,42 +1,48 @@
 import time
 import ssl
 import sys
+import os
 
 import click
 from rich.console import Console
 from rich.table import Table
 import paho.mqtt.client as mqtt
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
 from sniffs import Sniffs
 
+DEFAULT_DEVICES_TOPIC = "test/devices"
+
 console = Console()
 
-config = dotenv_values(".config")
-_location = config.get("LOCATION")
-if not _location:
-    console.print("[bold red]Error: Must include a [bold orchid2].config[/bold orchid2] file with [orange3]LOCATION[/orange3][bright_white]=[/bright_white][italic magenta]location[/italic magenta].")
-    sys.exit(1)
+print_settings_string = ""
 
-secrets = dotenv_values(".secrets")
-_user = secrets.get("MQTT_SERVER_USER")
-_password = secrets.get("MQTT_SERVER_PASS")
-_host = secrets.get("MQTT_SERVER_IP")
-_port = int(secrets.get("MQTT_SERVER_PORT") or 0)
-if not (_user and _password and _host and _port):
-    console.print("[bold red]Error: Must include a [bold orchid2].secrets[/bold orchid2] file with the following items:[/bold red]\n"
-                  "[orange3]MQTT_SERVER_USER[/orange3][bright_white]=[/bright_white][italic magenta]some_user[/italic magenta].\n"
-                  "[orange3]MQTT_SERVER_PASS[/orange3][bright_white]=[/bright_white][italic magenta]hunter2[/italic magenta].\n"
-                  "[orange3]MQTT_SERVER_IP[/orange3][bright_white]=[/bright_white][italic magenta]foo.com[/italic magenta].\n"
-                  "[orange3]MQTT_SERVER_PORT[/orange3][bright_white]=[/bright_white][italic magenta]1337[/italic magenta].")
-    sys.exit(1)
+def add_print_setting(setting_key: str, setting_value: str):
+    global print_settings_string
+    print_settings_string += f"[orange3]{setting_key}[/orange3][magenta]:[/magenta] [bright_white]{setting_value}[magenta];[/magenta] "
 
+load_dotenv(".config")
+_devices_topic = os.getenv("MQTT_DEVICES_TOPIC", DEFAULT_DEVICES_TOPIC)
+add_print_setting("MQTT_DEVICES_TOPIC", _devices_topic)
+
+secrets = load_dotenv(".secrets")
+_user = os.getenv("MQTT_SERVER_USER")
+if _user:
+    add_print_setting("MQTT_SERVER_USER", _user)
+_password = os.getenv("MQTT_SERVER_PASS")
+if _user and not _password:
+    console.print(f"[bold red]Error: Must set [orange3]MQTT_SERVER_PASSWORD[/orange3] if [orange3]MQTT_SERVER_USER[/orange3] is set.[/bold red]")
+    sys.exit(1)
+_host = os.getenv("MQTT_SERVER_HOST", "localhost")
+add_print_setting("MQTT_SERVER_HOST", _host)
+_port = int(os.getenv("MQTT_SERVER_PORT", 1883))
+add_print_setting("MQTT_SERVER_PORT", str(_port))
+
+console.print(print_settings_string)
 devices_dict = {}
-
 sniffs = Sniffs()
 
-
-@sniffs.route("<location>:{" + _location + "}/devices/<device_id>/<setting>")
+@sniffs.route(_devices_topic.rstrip("/").lstrip("/") + "/<device_id>/<setting>")
 def device_settings(device_id, setting, message):
     if isinstance(message, bytes):
         message = message.decode()
@@ -47,8 +53,9 @@ def device_settings(device_id, setting, message):
 
 def run_client() -> mqtt.Client:
     client = mqtt.Client(client_id="dops_client")
-    client.username_pw_set(_user, password=_password)
-    client.tls_set(cert_reqs=ssl.CERT_NONE)
+    if _user and _password:
+        client.username_pw_set(_user, password=_password)
+        client.tls_set(cert_reqs=ssl.CERT_NONE)
     client.connect(_host, _port)
     sniffs.bind(client)
     client.loop_start()
@@ -81,14 +88,7 @@ def auth():
 @click.argument("device_id", required=False)
 @click.argument("setting_id", required=False)
 @click.option("--set", "-s", "new_value", help="Value to publish/set.")
-@click.option(
-    "--type",
-    "-t",
-    "new_value_type",
-    type=click.Choice(["str", "bytes", "bytearray", "float"]),
-    help="Type of the published value. (will be cast)",
-)
-def devices(device_id, setting_id, new_value, new_value_type):
+def devices(device_id, setting_id, new_value):
     """
     Shows device settings, or updates a specific device setting.
 
@@ -106,26 +106,18 @@ def devices(device_id, setting_id, new_value, new_value_type):
         not is_command_get_device and not is_command_set_device_setting
     )
 
-    # Early validations before running client
-    # if --set or --type is provided, the other is expected
-    if (new_value or new_value_type) and not (new_value and new_value_type):
-        console.print(
-            f"When providing a new setting value, must specify both --set and --type options."
-        )
-        return
-
     # Call this from inside devices to delay running until options validation is done
     client = run_client()
 
     if is_command_list_devices:
         if not devices_dict.keys():
             console.print(
-                f"No devices found at [{Clr.loc}]{_location}[/{Clr.loc}]."
+                f"No devices found at [{Clr.loc}]{_devices_topic}[/{Clr.loc}]."
             )
             return
 
         table = Table()
-        table.add_column("Device", style=Clr.dev)
+        table.add_column("Devices", style=Clr.dev)
         for device_id in devices_dict.keys():
             table.add_row(device_id)
         console.print(table)
@@ -134,14 +126,14 @@ def devices(device_id, setting_id, new_value, new_value_type):
     device = devices_dict.get(device_id)
     if not device:
         console.print(
-            f"Device: [{Clr.dev}]{device_id}[/{Clr.dev}] not found at [{Clr.loc}]{_location}[/{Clr.loc}]."
+            f"Device: [{Clr.dev}]{device_id}[/{Clr.dev}] not found at [{Clr.loc}]{_devices_topic}[/{Clr.loc}]."
         )
         return
 
     if is_command_get_device_setting or is_command_set_device_setting:
         if not device.get(setting_id):
             console.print(
-                f"Device: [{Clr.dev}]{device_id}[/{Clr.dev}] and setting: [{Clr.set}]{setting_id}[/{Clr.set}] not found at [{Clr.loc}]{_location}[/{Clr.loc}]."
+                f"Device: [{Clr.dev}]{device_id}[/{Clr.dev}] and setting: [{Clr.set}]{setting_id}[/{Clr.set}] not found at [{Clr.loc}]{_devices_topic}[/{Clr.loc}]."
             )
             return
 
@@ -163,16 +155,7 @@ def devices(device_id, setting_id, new_value, new_value_type):
         console.print(table)
 
     elif is_command_set_device_setting:
-        if new_value_type == "float":
-            new_value = float(new_value)
-        elif new_value_type == "bytearray":
-            new_value = bytearray(new_value)
-        elif new_value_type == "bytes":
-            new_value = bytes(new_value)
-        elif new_value_type == "str":
-            new_value = str(new_value)
-
-        topic = f"{_location}/devices/{device_id}/{setting_id}"
+        topic = f"{_devices_topic}/{device_id}/{setting_id}"
         client.publish(topic, new_value, retain=True)
         client.loop_start()
         time.sleep(0.5)
